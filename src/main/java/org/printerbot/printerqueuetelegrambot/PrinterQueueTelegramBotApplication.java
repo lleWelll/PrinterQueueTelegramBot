c23ed5563815b83e3b522c207ae1593b69c77f2e
llewell <llwll@MacBoo.local>
2025-05-13 13:13:28 +0500

    Empty Project

---
+++ src/main/java/org/printerbot/printerqueuetelegrambot/PrinterQueueTelegramBotApplication.java
@@ -0,0 +1,13 @@
+package org.printerbot.printerqueuetelegrambot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrinterQueueTelegramBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrinterQueueTelegramBotApplication.class, args);
+	}
+
+}
