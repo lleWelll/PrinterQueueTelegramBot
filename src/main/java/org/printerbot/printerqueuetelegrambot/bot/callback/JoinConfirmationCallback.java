eef2f4c5003beb42593b98c9a5edef2913993582
llewell <llwll@MacBoo.local>
2025-05-16 01:31:52 +0500

    Working at adding skip button

---
+++ src/main/java/org/printerbot/printerqueuetelegrambot/bot/callback/JoinConfirmationCallback.java
@@ -0,0 +1,55 @@
+package org.printerbot.printerqueuetelegrambot.bot.callback;
+
+import lombok.RequiredArgsConstructor;
+import org.printerbot.printerqueuetelegrambot.bot.constants.CallbackType;
+import org.printerbot.printerqueuetelegrambot.bot.constants.ConstantMessages;
+import org.printerbot.printerqueuetelegrambot.bot.user.UserSessionManager;
+import org.printerbot.printerqueuetelegrambot.bot.util.JsonHandler;
+import org.printerbot.printerqueuetelegrambot.model.dto.QueueDto;
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+@RequiredArgsConstructor
+public class JoinConfirmationCallback implements Callback{
+
+	private final UserSessionManager sessionManager;
+
+	@Override
+	public SendMessage apply(String data, Update update) {
+
+		QueueDto session = sessionManager.getSession(getChatId(update));
+
+		String confirmationMessage = ConstantMessages.CONFIRM_JOIN_MESSAGE.getFormattedMessage(
+				session.getPrinter().getPrinterInfo(),
+				session.getPlastics().get(0).getPlasticInfo(),
+				session.getStlModelName() == null ? "Not uploaded" : session.getStlModelName()
+		);
+
+		SendMessage sendMessage = createSendMessage(update, confirmationMessage);
+		addKeyboard(sendMessage);
+		return sendMessage;
+	}
+
+	private void addKeyboard(SendMessage message) {
+		List<List<InlineKeyboardButton>> buttons = new ArrayList<>();
+
+		buttons.add(List.of(createButton(CallbackType.DONE_JOIN, "Yes")));
+		buttons.add(List.of(createButton(CallbackType.CANCEL_JOIN, "No")));
+
+		message.setReplyMarkup(new InlineKeyboardMarkup(buttons));
+	}
+
+	private InlineKeyboardButton createButton(CallbackType type, String text) {
+		return InlineKeyboardButton.builder()
+				.text(text)
+				.callbackData(JsonHandler.listToJson(List.of(type.toString(), text)))
+				.build();
+	}
+}
